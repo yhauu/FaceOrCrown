04cc361774125a01f017d6e3680a3089987ad846
Fabricio Belomo <fabriciobelomo@gmail.com>
2019-05-14 22:21:35 -0300

    Insercao de imagens na tela de resultado, ainda com erro na parte logica de selecionar a imagem

--- app/src/main/java/br/com/etecia/thegame/MainActivity.java
+++ app/src/main/java/br/com/etecia/thegame/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/br/com/etecia/thegame/ResultActivity.java
+++ app/src/main/java/br/com/etecia/thegame/ResultActivity.java
@@ -1,8 +1,12 @@
 package br.com.etecia.thegame;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import java.util.Random;
 public class ResultActivity extends AppCompatActivity {
 
     @Override
@@ -10,6 +14,17 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+        ImageView imageResult = findViewById(R.id.imageView3);
+        Random aleatorio = new Random();
+
+        int numAlea = aleatorio.nextInt(0-1);
+
+        if (numAlea == 1){
+            imageResult.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_coroa));
+        } else {
+            imageResult.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_cara));
+        }
 
     }
 }
